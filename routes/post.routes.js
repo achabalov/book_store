95b6ef3dd1b0cc1c52c3b75d70c54a68b878b1bf
Alexander Chabalov <a.chabalov@gmail.com>
2021-10-06 22:18:03 +0300

    backend node+postgresql
    need create new database & added  frontend with react

---
+++ routes/post.routes.js
@@ -0,0 +1,11 @@
+const Router = require('express');
+
+const router = new Router();
+
+const postController = require('../controllers/post.controller');
+
+router.post('/post', postController.createPost)
+router.get('/post', postController.getPost)
+
+
+module.exports = router;
